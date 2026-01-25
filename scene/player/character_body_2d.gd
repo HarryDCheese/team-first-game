bac9fc801c2fe662cdbc477b214731a9688449d7
BenMyQuick <duy.08.72.tonthattung@gmail.com>
2026-01-25 11:09:08 -0700

    Player Update
    Change folders arrangement
    Optimize script

---
+++ scene/player/character_body_2d.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+
+
+@export var speed := 200
+
+
+func _physics_process(delta: float) -> void:
+	var input_dir = Input.get_vector("move_left","move_right","move_up", "move_down")
+	velocity = input_dir * speed
+
+	move_and_slide()
